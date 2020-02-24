00f78ca0716c1e34adbe219ee0fc598b7bbb3f9e
Devin_Moore <d.j.moore209@gmail.com>
2020-02-24 15:53:37 -0500

    completed 3 questions

--- index.js
+++ index.js
@@ -39,9 +39,17 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
+
+function makePersonObject(idVal, nameVal, emailVal) {
+  return {
+    id: idVal,
+    name: nameVal,
+    email: emailVal
+  }
 }
+
+//var sam = makePersonObject(2343, "Sam", "email@email.com")
+//console.log(sam);
 
 /**
  * ### Challenge `getName`
@@ -56,10 +64,11 @@ function makePersonObject(/* code here */) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName(obj) {
+  return `Hello, my name is ${obj.name}`
 }
 
+//console.log(getName({id:1, name:"leia", email:"email@email.com"}))
 /**
  * ### Challenge `makeSmartPerson`
  * 
@@ -73,13 +82,9 @@ function getName(/* code here */) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
+function makeSmartPerson(nameInput) {
+  return {name: nameInput, sum: function(num1, num2){return num1 + num2},speak: function(){return `Hello, my name is ${this.name}`}}
 }
-
-
-
-
 
 /*
 // ⭐️ Example Test Data ⭐️
